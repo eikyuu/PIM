f4c16efa390c3ef2fc821f43055551c016b47926
Vincent DUGUET <vduguet@avidsen.com>
2020-02-14 17:13:21 +0100

    mail

--- Controller/ContactPimController.php
+++ Controller/ContactPimController.php
@@ -4,19 +4,32 @@
 namespace ContactPim\Controller;
 
 use Tags\Model\TagsQuery;
-use Thelia\Controller\Admin\BaseAdminController;
+use Thelia\Controller\Front\BaseFrontController;
 use Thelia\Model\ProductQuery;
 use Thelia\Model\ConfigQuery;
 
-class ContactPimController extends BaseAdminController
+class ContactPimController extends BaseFrontController
 {
-    public function sendEmail()
+    public function sendAction()
     {
-        $subject = "yep";
+
+        $subject = "Probleme sur le P.I.M";
+        
+        $text = $this->getRequest()->get("contact_pim");
+        $email = $this->getRequest()->get("email_contact");
+
         $message = \Swift_Message::newInstance($subject)
             ->addFrom(ConfigQuery::getStoreEmail())
             ->addTo("vduguet@avidsen.com")
-            ->setBody("message");
+            ->setBody($this->render("mail", 
+                [
+                    'text' => $text,
+                    'email'=> $email
+                ]), 
+                "text/html");
         $this->getMailer()->send($message);
+        header('Location: /');
+        exit();
+        
     }
 }
